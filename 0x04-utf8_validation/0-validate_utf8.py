7cbbab3b2d070facdbc3f736b1412dbe5c60f951
SafaFarah <kangjisook13@gmail.com>
2024-08-24 00:14:33 +0300

    debug 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,8 +9,24 @@ def validUTF8(data):
     data: List of integers where each integer represents a byte
     return: True if valid UTF-8 encoding, otherwise False
     """
-    try:
-        bytes(data).decode('utf-8')
-    except (ValueError, UnicodeDecodeError):
-        return False
-    return True
+    n_bytes = 0
+    for byte in data:
+        if byte < 0 or byte > 255:
+            return False
+        if n_bytes == 0:
+            if (byte & 0b10000000) == 0b00000000:
+                n_bytes = 0  # 1-byte character
+            elif (byte & 0b11100000) == 0b11000000:
+                n_bytes = 1  # 2-byte character
+            elif (byte & 0b11110000) == 0b11100000:
+                n_bytes = 2  # 3-byte character
+            elif (byte & 0b11111000) == 0b11110000:
+                n_bytes = 3  # 4-byte character
+            else:
+                return False  # Invalid start byte
+        else:
+            if (byte & 0b11000000) != 0b10000000:
+                return False  # Invalid continuation byte
+            n_bytes -= 1
+
+    return n_bytes == 0
