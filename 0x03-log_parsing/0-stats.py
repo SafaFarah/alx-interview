cce76bc16dae02ca2047c788b3031c33ad2844ba
SafaFarah <kangjisook13@gmail.com>
2024-09-27 00:42:52 +0300

    update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -28,7 +28,7 @@ def main():
                 total_file_size += file_size
                 if status_code in {200, 301, 400, 401, 403, 404, 405, 500}:
                     status_codes[status_code] = status_codes.get(
-                        status_code, 0) + 1
+                            status_code, 0) + 1
                 if line_count % 10 == 0:
                     print_statistics(total_file_size, status_codes)
         print_statistics(total_file_size, status_codes)
